9688c8e15787f80e3a1ea7a2ca7ebb7c108a54c9
kenglee52 < Gitcl>
2025-11-02 20:17:12 +0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({origin:'*'}));
 app.use('/uploads', express.static('uploads'));
 
 
